42d2aac2922ec9501d1c118db8efe7240586a5e2
wanghuifeng <wanghuifeng118@163.com>
2014-10-11 17:42:38 +0800

    first version, by rails new CloudMemo

---
+++ CloudMemo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CloudMemo::Application.config.secret_token = 'a162dda47aab242a2af9abbe114a7e1bde75ab56dc0d7c3591778bdfe7eeb2d6f95ef782ea32b5e4e6ba7f5a84a435929099b6e11cdfe4d02326df3f11b381fd'
