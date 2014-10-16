c7348ed82ffc6a0732e9ad89299e0fe5d3940f53
wanghuifeng <wanghuifeng118@163.com>
2014-10-17 00:32:08 +0800

    modify _form's HTML,change to chinese.

--- CloudMemo/app/controllers/comments_controller.rb
+++ CloudMemo/app/controllers/comments_controller.rb
@@ -1,11 +1,13 @@
 class CommentsController < ApplicationController
   def create
     @article = Article.find(params[:article_id])
-    @comment = @article.comments.create(comment_params)
+    @comment = @article.comments.create(params[:comment])
     redirect_to article_path(@article)
   end
-private
-  def comment_params
-    params.require(:comment).permit(:commenter,:body)
+ def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article)
   end
 end
